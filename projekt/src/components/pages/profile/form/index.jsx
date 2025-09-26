c4f1c560f5c4a43891c1ee18ce8ea907225f284b
milotobias <tobiastap@gmail.com>
2025-09-26 08:54:35 +0200

    Styled: Profile form
    fixed: profile action
    
    reverted: database file to backup oops

--- projekt/src/components/pages/profile/form/index.jsx
+++ projekt/src/components/pages/profile/form/index.jsx
@@ -5,29 +5,35 @@ import { useEffect } from "react";
 
 export default function ProfileForm({ user }) {
   const [formState, formAction, pending] = useActionState(profileAction);
+  
+  console.log(user)
+  
+  useEffect(() => {
+    console.log("formstate", formState)
+  }, [formState])
 
   return (
-    <form action={formAction}>
+    <form action={formAction} className="form">
       <h2>Update Profile</h2>
-      <div>
-        <label>
+      <div className="flex gap-4">
+        <label className="w-full">
           <span>First name</span>
           <input
             type="text"
             name="firstname"
             defaultValue={user.firstname}
           />
-          <span className="error-message"></span>
+          <span className="error-message">{formState?.properties?.firstname?.errors}</span>
         </label>
 
-        <label>
+        <label className="w-full">
           <span>Last name</span>
           <input
             type="text"
             name="lastname"
             defaultValue={user.lastname}
           />
-          <span className="error-message"></span>
+          <span className="error-message">{formState?.properties?.lastname?.errors}</span>
         </label>
       </div>
 
@@ -39,7 +45,7 @@ export default function ProfileForm({ user }) {
           name="email"
           defaultValue={user.email}
         />
-        <span className="error-message"></span>
+        <span className="error-message">{formState?.properties?.email?.errors}</span>
       </label>
 
       {/* Password */}
@@ -50,19 +56,18 @@ export default function ProfileForm({ user }) {
           name="password"
           placeholder="New password"
         />
-        <span className="error-message"></span>
+        <span className="error-message">{formState?.properties?.password?.errors}</span>
       </label>
 
       <button
         type="submit"
         disabled={pending}
+        className="button-primary py-2 w-full"
       >
         {pending ? "Updating..." : "Update profile"}
       </button>
 
       <p className="success-message"></p>
-
-      <input type="hidden" name="old_password" readOnly value={user.password} />
     </form>
 
   );
--- projekt/src/components/pages/profile/form/profile-action.js
+++ projekt/src/components/pages/profile/form/profile-action.js
@@ -4,28 +4,27 @@ import { revalidatePath } from "next/cache";
 import z from "zod";
 
 export default async function profileAction(prevState, formData) {
-  const { firstname, lastname, email, password, old_password} = Object.fromEntries(formData);
+  const { firstname, lastname, email, password } = Object.fromEntries(formData);
 
-  const newPassword = password?.trim() ? password : old_password
   const schema = z.object({
     firstname: z.string().min(1, { message: "First name is required" }),
     lastname: z.string().min(1, { message: "Last name is required" }),
-    email: z.email({ message: "Invalid email" }),
-    password: z.string().optional()
+    email: z.string().email({ message: "Invalid email" }),
+    password: z.string().min(1, { message: "Password is required" })
   });
 
-  const validated = schema.safeParse({ firstname, lastname, email, password: newPassword });
+  const validated = schema.safeParse({ firstname, lastname, email, password: password?.trim() });
 
   if (!validated.success) {
     return {
       ...validated,
       ...(z.treeifyError(validated.error)),
-      data: { firstname, lastname, email, password: newPassword }
+      data: { firstname, lastname, email, password }
     };
   }
 
-  const accessToken = await readCookie("sh_access_token")
-  const userId = await readCookie("sh_user_id")
+  const accessToken = await readCookie("sh_access_token");
+  const userId = await readCookie("sh_user_id");
 
   if (!accessToken || !userId) {
     return {
@@ -35,33 +34,31 @@ export default async function profileAction(prevState, formData) {
     };
   }
 
-  const payload = {
-    firstname: validated.data.firstname,
-    lastname: validated.data.lastname,
-    email: validated.data.email
-  };
-  if (validated.data.password) {
-    payload.password = validated.data.password;
-  }
-
   const response = await fetch(`http://localhost:4000/api/v1/users/${userId}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
       "Authorization": `Bearer ${accessToken}`
     },
-    body: JSON.stringify(payload)
+    body: JSON.stringify({
+      firstname: validated.data.firstname,
+      lastname: validated.data.lastname,
+      email: validated.data.email,
+      password: validated.data.password
+    })
   });
 
-  if (!response.ok) return {
-    success: false,
-    errors: ['Could not update profile. Try again later']
-  };
+  if (!response.ok) {
+    return {
+      success: false,
+      errors: ["Could not update profile. Try again later."]
+    };
+  }
 
-  revalidatePath('http://localhost:3000/profile')
+  revalidatePath("/profile");
 
   return {
     success: true,
-    message: 'Successfully updated profile!'
+    message: "Successfully updated profile!"
   };
 }
